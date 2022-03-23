aa57cb85077c53c273e8d2a2bd055ff96ba257fb
amed012 <a.elbaz@gmail.com>
2022-03-23 12:24:14 -0700

    add 3 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InscriptionComponent,
+    AcceuilComponent
   ],
   imports: [
     BrowserModule,
